e5d64933ae6a447878e03c31d6fda8de77822781
dhananjay <dhananjay.cdac@gmail.com>
2024-01-11 23:58:46 +0530

    commit for microservice async

---
+++ src/main/java/com/dhananjay/example/microserviceasync/MicroserviceasyncApplication.java
@@ -0,0 +1,13 @@
+package com.dhananjay.example.microserviceasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceasyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceasyncApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/dhananjay/example/microserviceasync/service/InventoryService.java
@@ -0,0 +1,15 @@
+package com.dhananjay.example.microserviceasync.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+@Slf4j
+public class InventoryService {
+
+    public boolean checkProductAvailability(int productId) {
+        return true;
+    }
+}
